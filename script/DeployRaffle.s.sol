6d19f1756660f99e842c38e43210760284dea3a1
Asad Hayat <asadhayat.1068@gmail.com>
2024-09-18 14:01:01 +0500

    Add consumer

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,18 +4,28 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
-import {SubscriptionFactory} from "script/Interactions.s.sol";
+import {SubscriptionFactory, FundSubscription, AddConsumer} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script {
-    function run() public {}
+    function run() public {
+      deployContract();
+    }
 
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
 
         HelperConfig.NetworkConfig memory networkConfig = helperConfig.getConfig();
         if (networkConfig.subscriptionId == 0) {
+          // Create a subscription
           SubscriptionFactory subscriptionFactory = new SubscriptionFactory();
           networkConfig.subscriptionId = subscriptionFactory.createSubscription(networkConfig.vrfCoordinator);
+          // Fund the subscription
+          FundSubscription fundSubscription = new FundSubscription();
+          fundSubscription.fundSubscription(
+            networkConfig.vrfCoordinator,
+            networkConfig.subscriptionId,
+            networkConfig.linkToken
+          );
         }
 
         vm.startBroadcast();
@@ -28,6 +38,9 @@ contract DeployRaffle is Script {
             networkConfig.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(address(raffle), networkConfig.vrfCoordinator, networkConfig.subscriptionId);
 
         return (raffle, helperConfig);
     }
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,6 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {HelperConfig, CodeConstants} from "script/HelperConfig.s.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 import {LinkToken} from "test/mocks/LinkToken.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract SubscriptionFactory is Script{
   function createSubscriptionUsingConfig() public returns (uint256 subscriptionId, address vrfCoordinator) {
@@ -62,5 +63,29 @@ contract FundSubscription is Script, CodeConstants {
 }
 
 contract AddConsumer is Script, CodeConstants {
-  function run() external {}
+  function addConsumerUsingConfig(address recentDeployment) public {
+    HelperConfig helperConfig = new HelperConfig();
+    address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+    uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
+    addConsumer(recentDeployment, vrfCoordinator, subscriptionId);
+  }
+
+  function addConsumer(
+    address _consumerContract,
+    address _vrfCoordinator,
+    uint256 _subscriptionId
+  ) public {
+    console2.log("Adding consumer to VRF Coordinator: ", _vrfCoordinator);
+    console2.log("Adding consumer to subscription id: ", _subscriptionId);
+    console2.log("Adding consumer to contract: ", _consumerContract);
+    console2.log("Adding consumer on chain id: ", block.chainid);
+    vm.startBroadcast();
+    VRFCoordinatorV2_5Mock(_vrfCoordinator).addConsumer(_subscriptionId, _consumerContract);
+    vm.stopBroadcast();
+  }
+
+  function run() external {
+    address mostRecentlyDeployedContract = DevOpsTools.get_most_recent_deployment("Raffle", block.chainid);
+    addConsumerUsingConfig(mostRecentlyDeployedContract);
+  }
 }
